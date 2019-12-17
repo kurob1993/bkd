ff522e1ec4d9516f51180079824cc65884efddbb
kurob <kurob1993@gmail.com>
2019-12-17 15:58:05 +0700

    expor pdf

--- app/Http/Controllers/TenagaKerja/HonorerController.php
+++ app/Http/Controllers/TenagaKerja/HonorerController.php
@@ -132,9 +132,9 @@ class HonorerController extends Controller
     public function pdf()
     {
         $emp = Employee::all();
-        // $pdf = PDF::loadview('honorer._pdf',compact('emp'));
-    	// return $pdf->download('laporan-pegawai-pdf.pdf');
-    	return view('honorer._pdf',compact('emp'));
+        $pdf = PDF::loadview('honorer._pdf',compact('emp'));
+    	return $pdf->download('laporan-pegawai-pdf.pdf');
+    	// return view('honorer._pdf',compact('emp'));
     }
 
     public function excel() 
